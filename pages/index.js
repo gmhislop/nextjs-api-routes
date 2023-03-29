b34734cb0ce728fa48e2d322f5884b83f970c68c
“gmhislop” <g.hislop@live.nl>
2023-03-29 13:48:44 +0200

    fix: added api routes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,41 @@
-function HomePage() {
+import { useRef } from 'react';
+
+const HomePage = () => {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = { email: enteredEmail, text: enteredFeedback};
+
+    fetch('/api/feedback/', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type='email' id='email'></input>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
-        <label htmlFor="feedback">Your Email Address</label>
-          <textarea id='feedback' rows='5'></textarea>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} />
         </div>
         <button>Send Feedback</button>
       </form>
     </div>
   );
-}
+};
 
 export default HomePage;
